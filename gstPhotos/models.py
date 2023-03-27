9a3118fad13cf834221d67f3cabafee49289201a
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-27 23:36:45 +0200

    init gstPhotos

--- gstPhotos/models.py
+++ gstPhotos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Photos(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_modification = models.DateTimeField(auto_now=True)
+    images = models.ImageField(upload_to='photos/')
+    extension = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nom)
+        super(Photos, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nom
+
+    class Meta:
+        ordering = ['-nom']
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+
+
+
+
+
+
--- gstPhotos/views.py
+++ gstPhotos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from gstPhotos.models import Photos
+
+
 # Create your views here.
+def gallery(request):
+    photos = Photos.objects.all()
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'gstPhotos/gallery.html', context)
