051d66e921e943d82e2aadd92787f2b349a88948
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-30 00:35:18 +0200

    a voir

--- gstDocuments/urls.py
+++ gstDocuments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from gstDocuments.views import DocumentsView, DocumentDetailsView
+from gstDocuments.views import DocumentsView, DocumentDetailsView, pdf_to_image
 
 urlpatterns = [
     path('documents-view/', DocumentsView.as_view(), name='gallery-doc-view'),
-    path('documents-details/<slug:slug>', DocumentDetailsView.as_view(), name='details-doc'),
+    path('documents-details/<slug:slug>/', DocumentDetailsView.as_view(), name='details-doc'),
+    path('pdf_to_image/<path:pdf_file>/', pdf_to_image, name='pdf_to_image'),
 ]
