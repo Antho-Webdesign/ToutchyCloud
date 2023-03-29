0c6a64b2ec62c5f6058172041a084a25fb64fb75
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-29 17:18:55 +0200

    refac link home + add items in bdd

--- gstDocuments/urls.py
+++ gstDocuments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from gstDocuments.views import DocumentsView
+from gstDocuments.views import DocumentsView, DocumentDetailsView
 
 urlpatterns = [
     path('documents-view/', DocumentsView.as_view(), name='gallery-doc-view'),
+    path('documents-details/<slug:slug>', DocumentDetailsView.as_view(), name='details-doc'),
 ]
--- gstDocuments/views.py
+++ gstDocuments/views.py
@@ -13,3 +13,15 @@ class DocumentsView(generic.ListView):
         # Add in a QuerySet of all the books
         context['documents'] = Documents.objects.all()
         return context
+
+
+class DocumentDetailsView(generic.DetailView):
+    model = Documents
+    template_name = 'documents/document_details.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        context['details-doc'] = Documents.objects.all()
+        return context
