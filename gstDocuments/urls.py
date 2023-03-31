8c0ea5e57e3927d7cc26663e107643b3f3112351
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-31 19:42:15 +0200

    design home photo

--- gstDocuments/urls.py
+++ gstDocuments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from gstDocuments.views import DocumentsView, DocumentDetailsView, pdf_to_image
+from gstDocuments.views import DocumentsView, DocumentDetailsView
 
 urlpatterns = [
     path('documents-view/', DocumentsView.as_view(), name='gallery-doc-view'),
     path('documents-details/<slug:slug>/', DocumentDetailsView.as_view(), name='details-doc'),
-    path('pdf_to_image/<path:pdf_file>/', pdf_to_image, name='pdf_to_image'),
+    # path('pdf_to_image/', pdf_to_image, name='pdf_to_image'),
 ]
--- gstDocuments/views.py
+++ gstDocuments/views.py
@@ -1,39 +1,19 @@
-from io import BytesIO
-
-from PIL import Image
-from pathlib import Path
-from django.http import HttpResponse
 from django.views import generic
-from pdf2image import convert_from_path
+from django.views.generic import ListView
 
 from .models import Documents
 
 
-def pdf_to_image(request, pdf_file, filename):
-    print(filename)
-    pages = convert_from_path(pdf_file)
-    print(pdf_file)
-    print(pages)
-    for i, page in enumerate(pages):
-        img_io = BytesIO()
-        page.save(img_io, 'JPEG')
-        img_io.seek(0)
-        img = Image.open(img_io)
-        file_path = Path(filename).with_suffix(f".{i}.jpg")
-        print("**********", filename, "**********")
-        img.save(file_path)
-
-
-class DocumentsView(generic.ListView):
-    print(pdf_to_image)
+class DocumentsView(ListView):
     queryset = Documents.objects.all()
     template_name = 'documents/home_documents.html'
+    context_object_name = 'documents_list'
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the books
-        context['documents'] = Documents.objects.all()
+
         return context
 
 
@@ -45,5 +25,5 @@ class DocumentDetailsView(generic.DetailView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the books
-        context['photo_list'] = Documents.objects.all()
+        context['documents'] = Documents.objects.all()
         return context
