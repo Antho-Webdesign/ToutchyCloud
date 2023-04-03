d5f030cbfb7952b3694ba0954e5f8d9779b936c8
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-04-03 20:36:23 +0200

    a revoir v2

--- gstPhotos/urls.py
+++ gstPhotos/urls.py
@@ -4,6 +4,5 @@ from gstPhotos.views import GalleryView, GalleryDetailView
 
 urlpatterns = [
     path('galleryview/', GalleryView.as_view(), name='gallery-view'),
-
-    path('details-view/<slug:slug>', GalleryDetailView.as_view(), name='img-details-view'),
+    path('details-view/<slug:slug>/', GalleryDetailView.as_view(), name='img-details-view'),
 ]
--- toutchycloud_settings/views.py
+++ toutchycloud_settings/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from gstDocuments.models import Documents
+from gstPhotos.models import Photos
+
 
 def home(request):
-    return render(request, 'home.html')
+    photos = Photos.objects.all()
+    documents = Documents.objects.all()
+
+    if name := request.GET.get('search'):
+        if request.method == 'GET':
+            photos = Photos.objects.filter(name__icontains=name)
+
+    if name := request.GET.get('search'):
+        if request.method == 'GET':
+            documents = Documents.objects.filter(name__icontains=name)
+
+    context = {
+        'documents': documents,
+        'photos': photos,
+    }
+    return render(request, 'home.html', context)
