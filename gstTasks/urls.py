9d30a840cd3c329979eaee2d8eac181a856cbe4f
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-04-17 14:02:53 +0200

    uninsrall libsaass

--- gstTasks/urls.py
+++ gstTasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from gstTasks.views import home_tasks
 
 urlpatterns = [
-    
+    path('gestion-taches/', home_tasks, name="home_tasks"),
 ]
--- gstTasks/views.py
+++ gstTasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_tasks(request):
+    return render(request, 'gstTasks/home_tasks.html')
