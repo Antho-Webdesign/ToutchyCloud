fa0e24001f2c7d18704127195da9725fb49ced1f
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-04-20 00:34:49 +0200

    modele rdv

--- gstRappels/admin.py
+++ gstRappels/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from gstRappels.models import Rdv
+
 # Register your models here.
+admin.site.register(Rdv)
--- gstRappels/models.py
+++ gstRappels/models.py
@@ -1,9 +1,8 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
-"""
-from django.db import models
-from django.contrib.auth.models import User
+from accounts.models import Customer
+
 
 class Rdv(models.Model):
     title = models.CharField(max_length=100)
@@ -12,18 +11,16 @@ class Rdv(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.CharField(max_length=100)
-    participants = models.ManyToManyField(User, related_name='appointments')
+    participants = models.ManyToManyField(Customer, related_name='appointments')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def save(self, *args, **kwargs):
-    self.slug = slugify(self.title)
-    super(Rdv, self).save(*args, **kwargs)
+        self.slug = slugify(self.title)
+        super(Rdv, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['start_time']
-
-"""
