77ec5c78aa2f46f251d7cc427870edb2a699fd2a
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-04-20 23:42:05 +0200

    modele rdv html v2 add cal view  ok in backend

--- gstRappels/views.py
+++ gstRappels/views.py
@@ -1,11 +1,10 @@
 from datetime import datetime, date
-
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
-
 from .models import *
 from .utils import Calendar
 
@@ -16,14 +15,8 @@ class CalendarView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # use today's date for the calendar
         d = get_date(self.request.GET.get('day', None))
-
-        # Instantiate our calendar class with today's year and date
         cal = Calendar(d.year, d.month)
-
-        # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
         return context
@@ -33,16 +26,16 @@ def get_date(req_day):
     if req_day:
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
-    return datetime
+    return datetime.now().date()  # return today's date if no date is specified
 
 
 def appointments(request):
     appointments = Appointment.objects.all()
     data = [
         {
-            'title': Appointment.title,
-            'start_time': appointment.start_time.isoformat(Appointment.start_time),
-            'end_time': appointment.end_time.isoformat(Appointment.end_time),
+            'title': appointment.title,
+            'start_time': appointment.start_time.isoformat(),
+            'end_time': appointment.end_time.isoformat(),
             'description': appointment.description
         }
         for appointment in appointments
@@ -51,28 +44,48 @@ def appointments(request):
 
 
 def calendar(request):
-    return render(request, 'gstRappels/calendar.html')
+    appointments = Appointment.objects.all()
+    rdv = Rdv.objects.all()
+    events = []
+    for event in Rdv.objects.all():
+        events.append({
+            'title': Rdv.title,
+            'start_time': Rdv.start_time,
+            'end_time': Rdv.end_time,
+        })
+
+    context = {
+        'appointment': appointments,
+        'rdv': rdv,
+        'event': event
+    }
+    return render(request, 'gstRappels/calendar.html', context)
+
+
+def events(request):
+    events = []
+    for event in Rdv.objects.all():
+        events.append({
+            'title': event.titre,
+            'start': event.date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'url': reverse('rdv_detail', args=[event.pk])
+        })
+    return JsonResponse(events, safe=False)
 
 
 def calendrier(request):
-    return render(request, 'gstRappels/appointement.html')
+    return render(request, 'gstRappels/appointment.html')
 
 
 @csrf_exempt
 def add_appointment(request):
     if request.method != 'POST':
-        # renvoyez une réponse JSON pour indiquer que la méthode de requête n'est pas autorisée
         return JsonResponse({'success': False, 'message': 'Méthode de requête non autorisée.'})
-    # récupérez les données de la requête POST
     user = request.user
     title = request.POST.get('title')
-    start_time = request.POST.get('start_time')
-    end_time = request.POST.get('end_time')
+    start_time = request.POST.get('start')
+    end_time = request.POST.get('end')
     description = request.POST.get('description')
-    # créez un nouvel objet Appointment avec les données
     appointment = Appointment(user=user, title=title, start_time=start_time, end_time=end_time, description=description)
-    # sauvegardez le rendez-vous dans la base de données
     appointment.save()
-    # renvoyez une réponse JSON pour indiquer que le rendez-vous a été ajouté avec succès
     return JsonResponse({'success': True})
-
