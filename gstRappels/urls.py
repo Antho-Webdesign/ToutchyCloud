c054811a1273373606f7e77e89320645ac25b30b
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-04-20 02:13:59 +0200

    modele rdv html

--- gstRappels/urls.py
+++ gstRappels/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from gstRappels.views import CalendarView
 
 urlpatterns = [
-    path('gestion-rdv/', CalendarView.as_view(), name="home_rdv"),
+    path('gestion-rdv/', CalendarView.as_view(), name="calendar"),
 ]
--- gstRappels/utils.py
+++ gstRappels/utils.py
@@ -13,19 +13,14 @@ class Calendar(HTMLCalendar):
     # filter events by day
     def formatday(self, day, events):
         events_per_day = events.filter(start_time__day=day)
-        d = ''
-        for event in events_per_day:
-            d += f'<li> {event.title} </li>'
-
+        d = ''.join(f'<li> {event.title} </li>' for event in events_per_day)
         if day != 0:
             return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
         return '<td></td>'
 
     # formats a week as a tr
     def formatweek(self, theweek, events):
-        week = ''
-        for d, weekday in theweek:
-            week += self.formatday(d, events)
+        week = ''.join(self.formatday(d, events) for d, weekday in theweek)
         return f'<tr> {week} </tr>'
 
     # formats a month as a table
