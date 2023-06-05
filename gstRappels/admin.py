864e1f3b98a5256b84f50abc4dc0b96572c70d11
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-06-05 23:52:00 +0200

    rappel models

--- gstRappels/admin.py
+++ gstRappels/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from gstRappels.models import Rappel
 
-# Register your models here.
+
+admin.site.register(Rappel)
--- gstRappels/models.py
+++ gstRappels/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Gestion des rappels
+from django.utils.text import slugify
+
+
+class Rappel(models.Model):
+    titre = models.CharField(max_length=255)
+    description = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_modification = models.DateTimeField(auto_now=True)
+    date_rappel = models.DateTimeField()
+    slug = models.SlugField(unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.titre)
+        super(Rappel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titre
+
+    class Meta:
+        ordering = ['-titre']
+        verbose_name = 'Rappel'
+        verbose_name_plural = 'Rappels'
